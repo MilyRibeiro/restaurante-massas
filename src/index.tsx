6149ec71ea89f8f6daee348b89df8190014c54f2
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2025-03-19 19:56:36 -0300

    Criando o menu

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-// import Cardapio from './pages/Cardapio';
-// import Inicio from 'pages/Inicio';
 import AppRouter from './routes';
-
-// const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,6 +11,5 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <AppRouter />
-    {/* {componenteAtual} */}
   </React.StrictMode>
 );
